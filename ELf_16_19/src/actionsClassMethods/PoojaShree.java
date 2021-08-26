f41bc483f369f5932d037d60b1aaac186ca81402
HP <HP@DESKTOP-1QBRB3K>
2021-08-26 10:40:44 +0530

    Pushing ActiTime

--- ELf_16_19/src/actionsClassMethods/PoojaShree.java
+++ ELf_16_19/src/actionsClassMethods/PoojaShree.java
@@ -1,5 +1,17 @@
 package actionsClassMethods;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class PoojaShree {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.actitime.com/");
+		driver.close();
+		
+	}
 
 }
